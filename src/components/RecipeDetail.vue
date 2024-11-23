4edbc186bcb9a2450b89fa39a656d299a79c5ec9
AxelChaikh <axelchaikh@gmail.com>
2024-11-23 17:27:09 -0300

    RecipeDetail dinamico

--- src/components/RecipeDetail.vue
+++ src/components/RecipeDetail.vue
@@ -1,64 +1,79 @@
 <script setup>
+import {onBeforeMount, onMounted, ref} from 'vue';
+import { useRoute } from 'vue-router';
+
+const route = useRoute();
+const recipeId = ref(route.params.id);
+const recetaAMostrar = ref()
+const recetaJSONGif = ref()
+
+console.log(recipeId.value);
+
+
+
+const traerPorID = async(id) => {
+  try{
+    let recetaBuscada = await fetch(`https://cookit-api.up.railway.app/recipes/recipes/${id}`)
+    let recetaJSON = await recetaBuscada.json()
+    recetaAMostrar.value = recetaJSON.recipe;
+    recetaJSONGif.value = recetaJSON.gif
+    await console.log(recetaAMostrar.value.name)
+  }catch {
+    console.error("Error al mostrar la receta:", error);
+  }
+
+}
+
+
+onBeforeMount(() => {
+   traerPorID(recipeId.value);
+});
 
 </script>
 
 <template>
 
-    <div class="container-lg text-center contenedor">
-        <h1 >Classic Margherita Pizza</h1>
+    <div class="container-lg text-center contenedor" >
+        <h1>{{ recetaAMostrar.name }}</h1>
         <!----- IMAGEN / INGREDIENTES / FICHA TÉCNICA ----->
-        <div class="row my-5 px-5 align-items-center">
+        <div class="row my-5 px-5 align-items-center" id="objetos">
             <!-- IMAGEN -->
             <div class="col-lg-6 col-md-12 col-sm-12 pb-3" >
-                <img src="/fotosRecetas/nioquis.png" class="img-fluid" alt="..." >
+                <img :src="`${recetaAMostrar.image}`" class="img-fluid" :alt="`${recetaAMostrar.name}`"  >
             </div>
             <!----- INGREDIENTES / FICHA TÉCNICA ----->
             <div class="col-lg-6 col-md-12 col-sm-12">
                 <div class="row">
                     <!-- INGREDIENTES -->
-                    <div class="col">
+                    <div class="col" id="ing">
                         <div class="card card-ing-ft">
                             <div class="card-body">
                                 <h5 class="card-title">INGREDIENTES</h5>
-                                <p class="card-text">
-
-                                    <br>
-                                    "Pizza dough",
-                                    <br>
-                                    "Tomato sauce",
-                                    <br>
-                                    "Fresh mozzarella cheese",
-                                    <br>
-                                    "Fresh basil leaves",
-                                    <br>
-                                    "Olive oil",
-                                    <br>
-                                    "Salt and pepper to taste"
-
+                                <p class="card-text" v-for="i in recetaAMostrar.ingredients">
+                                  - {{ i }}
                                 </p>
-
                             </div>
                         </div>
                     </div>
 
                     <!-- FICHA TÉCNICA -->
-                    <div class="col">
+                    <div class="col" id="ficha">
                         <div class="card card-ing-ft">
                         <div class="card-body">
                             <h5 class="card-title">FICHA TECNICA</h5>
-                            <p class="card-text">
+                            <p class="card-text" >
                                 <br>
-                                "prepTimeMinutes": 5,
+                                Minutos de preparación: {{ recetaAMostrar.prepTimeMinutes }}
                                 <br>
-                                "cookTimeMinutes": 0,
+                                Tiempo de cocción: {{ recetaAMostrar.cookTimeMinutes }}
                                 <br>
-                                "servings": 1,
+                                Cantidad de porciones: {{ recetaAMostrar.servings}}
                                 <br>
-                                "difficulty": "Easy",
+                                Dificultad: {{ recetaAMostrar.difficulty }}
                                 <br>
-                                "cuisine": "Brazilian",
+                                Cocina: {{ recetaAMostrar.cuisine }}
                                 <br>
-                                "caloriesPerServing": 150,
+                                Calorias por porción: {{ recetaAMostrar.caloriesPerServing }}
                             </p>
                         </div>
                     </div>
@@ -69,24 +84,17 @@
             </div>
         </div>
         <div class="row g-2">
-            <div class="col">
+            <div class="col" id="instrucciones">
                 <h2>INSTRUCCIONES</h2>
-                <p>
-                    <br>
-                    "Preheat the oven to 475°F (245°C).",
-                    <br>
-                    "Roll out the pizza dough and spread tomato sauce evenly.",
-                    <br>
-                    "Top with slices of fresh mozzarella and fresh basil leaves.",
-                    <br>
-                    "Drizzle with olive oil and season with salt and pepper.",
-                    <br>
-                    "Bake in the preheated oven for 12-15 minutes or until the crust is golden brown.",
-                    <br>
-                    "Slice and serve hot."
-
+                <p v-for="inst in recetaAMostrar.instructions">
+                    {{ inst }}
                 </p>
             </div>
+          <div class="col" id="gif">
+
+            <img :src="`${recetaJSONGif}`" class="gif">
+
+          </div>
         </div>
     </div>
 
@@ -97,6 +105,28 @@
 .card-ing-ft{
     width: auto;
     
+}
+
+#instrucciones{
+  margin-bottom: 80px;
+}
+
+.img-fluid {
+  height: 450px;
+  border-radius: 10%;
+}
+
+#ficha {
+  padding: 40px;
+
+}
+
+#ing {
+  padding: 40px;
+}
+
+.gif {
+  border-radius: 5%;
 }
 
 </style>
